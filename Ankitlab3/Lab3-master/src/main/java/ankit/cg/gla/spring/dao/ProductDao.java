4b8f4245632f39659940887d293b7a287a786fda
L-O-O-p <33959913+L-O-O-p@users.noreply.github.com>
2020-04-29 22:15:44 +0530

    Add files via upload

---
+++ Ankitlab3/Lab3-master/src/main/java/ankit/cg/gla/spring/dao/ProductDao.java
@@ -0,0 +1,12 @@
+package ankit.cg.gla.spring.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ankit.cg.gla.spring.entity.Product;
+
+
+
+public interface ProductDao extends JpaRepository<Product, Integer> {
+
+}
